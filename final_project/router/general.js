a5f170b8554d22ae21395b111dda4f64a59f23b4
GerOBrien021 <gerard.obrien.cork@gmail.com>
2026-04-01 10:43:07 -0400

    add general routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,21 +11,28 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function(req, res) {
+  res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn',function(req, res) {
+  const isbn = req.params.isbn;
+
+  res.send(books[isbn]);
+});
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+
+  // Get the keys of the books object
+  const keys = Object.keys(books);
+
+  // Filter the books object to get any matches by author
+  let books_by_author = keys.filter((key) => books[key].author === author);
+
+  res.send(books_by_author);
 });
 
 // Get all books based on title
